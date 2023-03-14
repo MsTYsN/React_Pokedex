78aff299d36acdbc76bc14433d92349151f2c0ec
Yassine Mestaoui <yassinemestaoui98@gmail.com>
2023-03-14 14:50:06 +0100

    cards done with styling + search is working

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import './App.css';
+import Pokemon from './components/Pokemon';
+import axios from 'axios';
+import { useEffect, useReducer, useState } from 'react';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        pokemonList: action.payload,
+        filterList: action.payload,
+      };
+    case 'FETCH_ERROR':
+      return { ...state, loading: false, error: action.payload };
+    case 'FILTER_SUCCESS':
+      return { ...state, loading: false, filterList: action.payload };
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [{ pokemonList, filterList, loading, error }, dispatch] = useReducer(
+    reducer,
+    {
+      pokemonList: [],
+      filterList: [],
+      loading: false,
+      error: '',
+    }
+  );
+
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(
+          'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0'
+        );
+        //console.log(data);
+        dispatch({ type: 'FETCH_SUCCESS', payload: data.results });
+      } catch (error) {
+        dispatch({ type: 'FETCH_SUCCESS', payload: error });
+      }
+    };
+    fetchData();
+  }, []);
+
+  // if (pokemonList == null) {
+  //   return;
+  // }
+
+  const searchHandler = () => {
+    dispatch({
+      type: 'FILTER_SUCCESS',
+      payload:
+        search !== ''
+          ? pokemonList.filter((p) =>
+              p.name.toLowerCase().includes(search.toLowerCase())
+            )
+          : pokemonList,
+    });
+  };
+
+  return (
+    <div className="container">
+      <input
+        type="text"
+        placeholder="Search A Pokemon By Name"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={searchHandler}>Search</button>
+      <br />
+      <div className="cards-container">
+        {loading ? (
+          <div className="loading-circle"></div>
+        ) : error ? (
+          <div className="error">Error</div>
+        ) : (
+          filterList.map((p, index) => <Pokemon item={p} key={index} />)
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,115 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import './Pokemon.css';
+
+function Pokemon({ item }) {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      //console.log(item.url);
+      const { data } = await axios.get(item.url);
+      setPokemon(data);
+      //console.log(data);
+    };
+    fetchData();
+  }, [item.url]);
+
+  if (pokemon == null) {
+    return;
+  }
+
+  return (
+    <div className="card">
+      <img
+        src={pokemon.sprites.other['official-artwork'].front_default}
+        alt={pokemon.name}
+      />
+      <h3>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
+      <div className="types">
+        {pokemon.types.map((t, index) => (
+          <span
+            key={index}
+            style={{
+              background:
+                t.type.name.toLowerCase() === 'normal'
+                  ? '#aa9'
+                  : t.type.name.toLowerCase() === 'fire'
+                  ? '#f42'
+                  : t.type.name.toLowerCase() === 'water'
+                  ? '#39f'
+                  : t.type.name.toLowerCase() === 'electric'
+                  ? '#fc3'
+                  : t.type.name.toLowerCase() === 'grass'
+                  ? '#7c5'
+                  : t.type.name.toLowerCase() === 'ice'
+                  ? '#6cf'
+                  : t.type.name.toLowerCase() === 'fighting'
+                  ? '#b54'
+                  : t.type.name.toLowerCase() === 'poison'
+                  ? '#a59'
+                  : t.type.name.toLowerCase() === 'ground'
+                  ? '#db5'
+                  : t.type.name.toLowerCase() === 'flying'
+                  ? '#89f'
+                  : t.type.name.toLowerCase() === 'psychic'
+                  ? '#f59'
+                  : t.type.name.toLowerCase() === 'bug'
+                  ? '#ab2'
+                  : t.type.name.toLowerCase() === 'rock'
+                  ? '#ba6'
+                  : t.type.name.toLowerCase() === 'ghost'
+                  ? '#66b'
+                  : t.type.name.toLowerCase() === 'dragon'
+                  ? '#76e'
+                  : t.type.name.toLowerCase() === 'dark'
+                  ? '#754'
+                  : t.type.name.toLowerCase() === 'steel'
+                  ? '#aab'
+                  : '#e9e',
+            }}
+          >
+            {t.type.name}
+          </span>
+        ))}
+      </div>
+      <div className="pokemon-stats">
+        {pokemon.stats.map((s, index) => (
+          <div className="stat" key={index}>
+            <div className="stat-label">
+              {s.stat.name === 'special-attack'
+                ? 's-attack'
+                : s.stat.name === 'special-defense'
+                ? 's-defense'
+                : s.stat.name}
+            </div>
+            <progress value={s.base_stat + s.effort} max="255"></progress>
+            <span className="stat-value">{s.base_stat + s.effort}</span>
+          </div>
+        ))}
+        {/* <div class="stat">
+          <div class="stat-label">Attack</div>
+          <progress value="85" max="255"></progress>
+        </div>
+        <div class="stat">
+          <div class="stat-label">Defense</div>
+          <progress value="70" max="255"></progress>
+        </div>
+        <div class="stat">
+          <div class="stat-label">Special Attack</div>
+          <progress value="90" max="255"></progress>
+        </div>
+        <div class="stat">
+          <div class="stat-label">Special Defense</div>
+          <progress value="80" max="255"></progress>
+        </div>
+        <div class="stat">
+          <div class="stat-label">Speed</div>
+          <progress value="95" max="255"></progress>
+        </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default Pokemon;
